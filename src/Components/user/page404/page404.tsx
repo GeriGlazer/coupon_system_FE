968999189107689a1d46af32dcd28d7a456d20d4
yoavhacmon <yoavh1234@gmail.com>
2022-07-06 12:29:35 +0300

    page404

--- src/Components/user/page404/page404.tsx
+++ src/Components/user/page404/page404.tsx
@@ -1,9 +1,10 @@
 import "./page404.css";
+import pic from "/Users/yoavhacmon/Desktop/coupon_system/src/assets/page404.png";
 
 function Page404(): JSX.Element {
     return (
         <div className="page404">
-			
+            <img src = {pic} width="75%" />
         </div>
     );
 }
